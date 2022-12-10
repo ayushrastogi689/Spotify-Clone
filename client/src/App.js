49b8bbfe1f17cac0469d6d3a0b5172fe45379ddd
Ayush Rastogi <ayushrastogi689@gmail.com>
2022-12-10 23:49:17 +0530

    Added Login component in App

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './Login';
+import Dashboard from './Dashboard';
+
+const code = new URLSearchParams(window.location.search).get('code') 
 
 function App() {
   return (
     <div className="App">
-      
+      {code ? <Dashboard code={code} /> :<Login />}
     </div>
   );
 }
